c0bdd5c11cd2d4537071f061086f1fca92ad55a2
Charanjit Singh <ckhabra007@gmail.com>
2015-08-30 07:22:32 +0530

    Make it write toast for post

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import {Observable} from 'rx';
 import moment from 'moment';
-import _ from lodash;
+import _ from 'lodash';
+import path from 'path';
+import {readFile, writeFile, existsSync, mkdir} from 'fs';
+import {copy} from 'fs-extra';
+
+const readFile_ = Observable.fromNodeCallback(readFile);
+const writeFile_ = Observable.fromNodeCallback(writeFile);
+const postTemplate = path.resolve(__dirname, 'assert/templates/post.html');
+
+const toastDir = path.resolve('./toast');
+
+const setupToastDir_ = () => {
+  if (existsSync(toastDir)) return Observable.return();
+
+  return Observable.fromNodeCallback(mkdir)(toastDir)
+    .merge(
+      Observable.fromNodeCallback(copy)(path.resolve(__dirname, 'assert/dist'), path.resolve(toastDir))
+    );
+};
 
 const cook = function materialCook(content_) {
   content_ = content_.share();
@@ -10,27 +28,55 @@ const cook = function materialCook(content_) {
         .map(content => ({
           content: content.comments,
           code: content.code,
+          meta: content.meta,
         }));
 
   let testsContent_ = content_
-        .filter(content => content.meta.type === 'tests');
+        .filter(content => content.meta.type === 'tests')
+        .map(content => ({
+          meta: content.meta
+        }));
 
   let postMeta_ = content_.take(1).pluck('meta');
 
-  Observable.merge(
-    testsContent_,
-    postContent_
-  ).combineLatest(
-    postMeta_,
-    (content, postMeta) => ({
-      title: postMeta.title,
-      publishedOn: new Date(),
-      post: content.content,
-      code: content.code,
-    })
-  )
+  let postHtml_ = Observable
+        .combineLatest(
+          postContent_,
+          postMeta_,
+          readFile_(postTemplate),
+          (content, postMeta, rawTemplate) => ({
+            context: {
+              title: content.meta.section,
+              subtitle: postMeta.subtitle || '',
+              authorName: postMeta.author || '',
+              publishedOn: new Date(),
+              post: content.content,
+              code: content.code,
+            },
+            rawTemplate,
+          })
+        )
+        .map(rawPost => ({
+          title: rawPost.context.title,
+          html: _.template(rawPost.rawTemplate)(rawPost.context),
+        }));
+
+  let generatedPostsWrites_ = postHtml_
+        .map(({html, title}) => ({
+          filepath: path.resolve(toastDir, `${title}.html`),
+          filecontent: html,
+        }))
+        .combineLatest(
+          setupToastDir_(toastDir),
+          (fileData) => fileData
+        )
+        .flatMap(({filepath, filecontent}) => {
+          return writeFile_(filepath, filecontent, 'utf-8');
+        });
+
+  generatedPostsWrites_
     .subscribe(
-      content => console.log(content),
+      x => console.log(),
       error => console.error(error),
       () => console.log('Done! Cook is ready')
     );
